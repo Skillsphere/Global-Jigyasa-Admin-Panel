06a0ddd3e99d366314adc304f8319ca199eea1d5
Dhiraj Das <dhiraj.das.05@gmail.com>
2021-05-22 23:32:33 +0530

    changed backend to use gj-dev app backend

--- projects/demo/src/environments/environment.prod.ts
+++ projects/demo/src/environments/environment.prod.ts
@@ -1,13 +1,21 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyCOVDlE5X7oehBLnRQ964N5_U2sbgwXiEU",
-    authDomain: "fir-cms-f4781.firebaseapp.com",
-    databaseURL: "https://fir-cms-f4781.firebaseio.com",
-    projectId: "fir-cms-f4781",
-    storageBucket: "fir-cms-f4781.appspot.com",
-    messagingSenderId: "1005426694599",
-    appId: "1:1005426694599:web:240b0855e34db99ef221e2",
-    measurementId: "G-RLK05DLV9Y"
+    apiKey: "AIzaSyDHW3uZ37E_NB2LAKCAiEsoDrN0J_moGjY",
+    authDomain: "global-jigyasa-dev.firebaseapp.com",
+    databaseURL: "https://global-jigyasa-dev.firebaseio.com",
+    projectId: "global-jigyasa-dev",
+    storageBucket: "global-jigyasa-dev.appspot.com",
+    messagingSenderId: "1033467886296",
+    appId: "1:1033467886296:web:6b2ab2b9fb23e9028735ad",
+    measurementId: "G-3YPGWN465K"
+    // apiKey: "AIzaSyCOVDlE5X7oehBLnRQ964N5_U2sbgwXiEU",
+    // authDomain: "fir-cms-f4781.firebaseapp.com",
+    // databaseURL: "https://fir-cms-f4781.firebaseio.com",
+    // projectId: "fir-cms-f4781",
+    // storageBucket: "fir-cms-f4781.appspot.com",
+    // messagingSenderId: "1005426694599",
+    // appId: "1:1005426694599:web:240b0855e34db99ef221e2",
+    // measurementId: "G-RLK05DLV9Y"
   }
 };
